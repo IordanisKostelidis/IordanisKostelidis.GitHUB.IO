675afad947ef1fbea99f1052997d09b08cf55b50
Iordanis Kostelidis <contact@iordaniskostelidis.eu>
2021-04-21 21:12:52 +0300

    change title, add custom favicon and add coming soon message

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <div style={{
+            textAlign: "center",
+
+        }}>
+            <div>Iordanis Kostelidis</div>
+            <div>Software Engineer</div>
+            <hr/>
+            <span>
+            Coming soon..., until then, check my <a href={"https://github.com/IordanisKostelidis"}>GitHub Profile</a>
+        </span>
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
